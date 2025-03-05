2125beda788bda7dfe30fea4675b0f413f6c00da
MdIsrakulIslam <israkulislam@gmail.com>
2025-03-05 12:41:33 +0600

    update

--- src/Components/Bannar/Bannar.jsx
+++ src/Components/Bannar/Bannar.jsx
@@ -3,28 +3,14 @@ import image1 from '../../assets/banner.jpg'
 
 export default function Bannar() {
   return (
-    <div>
-        <div className=" relative justify-center pt-36 boder rounded-3xl  bg-[#9538E2] w-[1160px] h-[694px]  ">
-  <div className="hero-content text-center">
-    <div className=" text-white p-8">
-      <h1 className="text-5xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
-      <p className="py-6">
-      Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
-      </p>
-      <button className="btn border rounded-full text-[#9538E2] bg-white ">Shop Now</button>
-    </div>
+    <div className='flex flex-col items-center py-10 px-4 sm:px-6 lg:px-8'>
+  <div className='relative bg-[#9538E2] text-white text-center rounded-3xl p-10 w-full max-w-6xl '>
+    <h1 className='text-3xl sm:text-4xl lg:text-5xl font-bold'>Upgrade Your Tech Accessories with Gadget Heaven</h1>
+    <p className='py-6'>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+    <button className='btn border rounded-full text-[#9538E2] bg-white'>Shop Now</button>
   </div>
+  <img className='relative -mt-8 sm:-mt-8 w-full max-w-lg border rounded-3xl bg-white p-4 shadow-lg' src={image1} alt='' />
+  <h3 className='font-bold text-2xl sm:text-3xl text-center pt-10'>Explore Cutting-Edge Gadgets</h3>
 </div>
-
-   <img className='relative flex justify-center -mt-40 w-[700px] h-[463px] ml-60 boder rounded-3xl bg-[#FFFFFF] p-4 shadow-transparent' src={image1} alt="" />
-
-
-   <h3 className='font-bold text-3xl justify-center ml-96 pt-10'>Explore Cutting-Edge Gadgets</h3>
-   <div>
-    <div>
-
-    </div>
-   </div>
-    </div>
   )
 }
